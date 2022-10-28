19339def6ac20f328a5c93664b8bdb615e0a33b8
Thanh Cong <congb1910622@student.ctu.edu.vn>
2022-10-28 18:20:36 +0700

    Create a contact listing page

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+
+  <div class="container mt-3">
+    <router-view />
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
+
+<script>
 export default {
 
 
